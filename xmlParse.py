7acb3c876a5e154eae2e551d02db35057afdc560
Euclidophren <48007660+Euclidophren@users.noreply.github.com>
2020-01-29 22:30:38 +0300

    Add files via upload

--- xmlParse.py
+++ xmlParse.py
@@ -1,6 +1,74 @@
 import xml.etree.ElementTree as et
 
 
+def get_stats_str(lst):
+    result = [str(i) + ':' + str(k) + '\n' for i in list(lst.keys()) for k in list(lst.values())]
+    result = list(set(result))
+    return ''.join(result)
+
+
+def get_os_family(root):
+    system_info = root.find("sys")
+    if system_info is None:
+        return ''
+    for info in system_info:
+        if info.tag == "os_family":
+            return info.text
+
+
+def get_processor_description(root):
+    proc_info = root.find("processor/item")
+    if proc_info is None:
+        return ''
+    for info in proc_info:
+        if info.tag == "description":
+            return info.text
+
+
+def get_memory_size(root):
+    mem_info = root.find("memory")
+    if mem_info is None:
+        return '0'
+    size = 0
+    for info in mem_info:
+        if info.tag == "size":
+            size += int(info.text)
+    return str(size)
+
+
+def get_video_card_model_and_size(root):
+    video_info = root.find("video/item")
+    if video_info is None:
+        return '', ''
+    manufacturer = []
+    for info in video_info:
+        if info.tag == "manufacturer":
+            manufacturer.append(info.text)
+    return manufacturer
+
+
+def get_audio_card_model(root):
+    sound_info = root.find("sound/item")
+    if sound_info is None:
+        return ''
+    for info in sound_info:
+        if info.tag == "model":
+            return info.text
+
+
+def parse_data(filename):
+    tree = et.parse(filename)
+    root = tree.getroot()
+
+    os_family = get_os_family(root)
+    proc_description = get_processor_description(root)
+    video_card = get_video_card_model_and_size(root)
+    audio_card = get_audio_card_model(root)
+
+    return os_family, proc_description, \
+           video_card, audio_card
+
+
 def parse_sys_info(root):
     system_info = root.find("sys")
 
@@ -10,7 +78,6 @@ def parse_sys_info(root):
     sys_data = []
 
     for info in system_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
         if info.text is not None:
             sys_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -28,7 +95,6 @@ def parse_processor_info(root):
     processor_data = []
 
     for info in processor_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
         if info.text is not None:
             processor_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -38,17 +104,16 @@ def parse_processor_info(root):
 
 
 def parse_memory_info(root):
-    memory_info = root.find("memory/item")
+    memory_info = root.find("memory")
 
     if memory_info is None:
         return ''
 
     memory_data = []
-
-    for info in memory_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
-        if info.text is not None:
-            memory_data.append(str(info.tag + ":" + info.text + '\n'))
+    for info in memory_info.findall("item"):
+        for item in info:
+            print(item.tag, item.text)
+            memory_data.append(str(item.tag + ":" + item.text + '\n'))
 
     result = ''.join(memory_data)
 
@@ -64,7 +129,6 @@ def parse_motherboard_info(root):
     motherboard_data = []
 
     for info in motherboard_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
         if info.text is not None:
             motherboard_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -81,7 +145,6 @@ def parse_video_card_info(root):
     video_card_data = []
 
     for info in video_card_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
         if info.text is not None:
             video_card_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -99,7 +162,6 @@ def parse_sound_info(root):
     sound_data = []
 
     for info in sound_info:
-        print(info.tag, info.text, sep=' : ', end='\n')
         if info.text is not None:
             sound_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -118,7 +180,6 @@ def parse_ip_info(root):
 
     for item in ip_info:
         for info in item:
-            print(item.tag, item.text, sep=' : ', end='\n')
             if info.text is not None:
                 ip_data.append(str(info.tag + ":" + info.text + '\n'))
 
@@ -159,7 +220,5 @@ def parse_client_response(filename):
     network_data = parse_ip_info(root)
 
     return sys_data, processor_data, memory_data, \
-        motherboard_data, sound_data, video_card_data, \
-        network_data
-
-
+           motherboard_data, sound_data, video_card_data, \
+           network_data
